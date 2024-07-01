082be72d9389436acb5c01ce030bda057f9613ac
Donald Ajaps <ajapon@yahoo.com>
2024-07-01 16:52:18 +0100

    Create Controllers: Auxiliary
    Add `api/controllers_auxiliary.py`. This is the first set of controllers for web server/API routes. Helper functions to provide various functionality for the other controllers.
    
    + Create `get_signed_id` function to read and decrypt player_id from cookies sent in a HTTP request. Used for user authentication
    + Create `signed_response` function to encrypt player_id and attach it as a cookie in a HTTP response object. The created cookie is persistent, and should be stored on the user's device for a week
    + Create `delete_player_id` function to remove invalid/expired session cookies from the user's device.
    + Create `favicon` function to explicitly serve a favicon for routes that don't specify one (typically the API text-only routes)
    + Create `error_handler` function to send generic messages for common server errors
    * Move `redirect_internal` function here from `api/api.py`.

---
+++ api/controllers_auxiliary.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""Contains helper functions for Anagram Master's controllers"""
+from flask import jsonify, redirect, make_response, send_from_directory
+from models import Cypher
+
+
+def get_player_id(req) -> str | None:
+    """Reads and decrypts player_id cookie from an HTTP request object"""
+    try:
+        player_id = req.cookies.get('session')
+        if player_id:
+            return Cypher.decrypt(player_id)
+    except Exception:
+        pass
+
+
+def signed_response(payload, player_id: str):
+    """HTTP response object with payload and encrypted player_id cookie"""
+    try:
+        resp = make_response(payload)
+        week = 60 * 60 * 24 * 7
+        resp.set_cookie(key='session', value=Cypher.encrypt(player_id),
+                        max_age=week, path='/', httponly=True)
+        return resp
+    except Exception as err:
+        return error_handler(500)
+
+
+def delete_player_id(payload):
+    """HTTP response object with payload and signal to remove encrypted
+    player_id cookie from the player's device"""
+    try:
+        resp = make_response(payload)
+        resp.delete_cookie(key='session', path='/', httponly=True)
+        return resp
+    except Exception as err:
+        return error_handler(500)
+
+
+def favicon():
+    """Explicitly serves a favicon for routes that don't specify one"""
+    return send_from_directory('static/media', 'logo_icon.png',
+                               mimetype='image/x-icon')
+
+
+def error_handler(e):
+    """Sends generic messages for common server errors"""
+    try:
+        code = e if isinstance(e, int) else e.code
+    except Exception:
+        code = 501
+    match code:
+        case 400:
+            return jsonify({'error': f'{code}: Bad Request'}), code
+        case 401:
+            return jsonify({'error': f'{code}: Unauthorised'}), code
+        case 403:
+            return jsonify({'error': f'{code}: Forbidden'}), code
+        case 404:
+            return jsonify({'error': f'{code}: Not Found'}), code
+        case 405:
+            return jsonify({'error': f'{code}: Method Not Allowed'}), code
+        case 500:
+            return jsonify({'error': f'{code}: Internal Server Error'}), code
+        case _:
+            return jsonify({'error': f'501: Not Implemented [{code}]'}), 501
+
+
+def redirect_internal(page: str):
+    """Redirects *.htm routes to corresponding route if available"""
+    if page == 'index':
+        return redirect('/', code=302)
+    if page == 'about' or page == 'scores' or page == 'game':
+        return redirect(f'/{page}', code=302)
+    return error_handler(404)
