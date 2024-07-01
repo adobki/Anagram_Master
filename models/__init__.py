59baf14792cb22e2c00765afea0c29efbbff7bfa
Donald Ajaps <ajapon@yahoo.com>
2024-07-01 07:44:28 +0100

    Add model with DBScores class for managing scores
    `models/__init__.py`:
    + Import and initialise an instance of the new model/class
    + Create `highscores` helper function for fetching highscores from the database, and print the highscores count when script executes.
    
    `models/db_scores.py`:
    + Create DBScores class for database abstraction of the scores database. Handles CRUD operations with data validation.

--- models/__init__.py
+++ models/__init__.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 """ Initializes the models package """
 from .game_engine import Game
+from .db_scores import DBScores
 
 
-
-def getScores():
-    """Loads list of high scores from storage."""
-    return fs.load('scores')
-
-
-def addScore(name: str, score: int):
-    """Saves list of high scores to storage."""
-    # Data Validation
-    if not isinstance(name, str) or not isinstance(score, int):
-        err = {'error': 'ERROR: name must be a string, score must be an int'}
-        print(err)
-        return err
-    if score < 1 or score > 9999:
-        err = {'error': 'ERROR: Invalid score! Must be >= 1 and <=9999'}
-        print(err)
-        return err
-    scores = fs.load('scores')
-    score = (score, name)
-    if score not in scores:
-        scores.append(score)
-        scores.sort(reverse=True)
-        scores = scores[:20] if len(scores) > 20 else scores
-        scores = fs.save('scores', scores)
+def highscores(full: bool = False) -> list[tuple]:
+    """Helper functon for getting highscores from the database"""
+    limit = 0 if full else 20
+    try:
+        scores = DBScores().load(limit=limit)
+    except Exception:
+        scores = []
     return scores
+
+
+print(f' highscores: {len(highscores(True))}\n\n')
---
+++ models/db_scores.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Highscores class for MongoDB database abstraction"""
+from datetime import datetime
+from utils import db, isAlive, DESCENDING
+
+
+class DBScores:
+    """Highscores class"""
+
+    @staticmethod
+    def isAlive() -> bool:
+        """Checks MongoDB database connection status"""
+        if not isAlive():
+            raise ConnectionError('MongoDB database connection failed!')
+        return True
+
+    def __init__(self):
+        """Set private attributes for new instance"""
+        self.__db = db['scores']
+
+    def load(self, limit: int = 0) -> list[tuple]:
+        """Loads specified number of top highscores scores from the database"""
+        self.isAlive()  # Check connection status before proceeding
+
+        sort = [('score', DESCENDING), 'date']
+        scores = []
+        for doc in self.__db.find().sort(sort).limit(limit):
+            try:
+                scores.append((doc['score'], doc['name'], doc['date']))
+            except KeyError:
+                continue  # Skip invalid database record
+        return scores
+
+    def add(self, name: str, score: int) -> None:
+        """Adds a player's score to the database"""
+        self.isAlive()  # Check connection status before proceeding
+
+        # Data validation
+        if not isinstance(name, str):
+            error = 'ERROR: name must be a string'
+            raise ValueError(error)
+        if not isinstance(score, int) or score < 1:
+            error = 'ERROR: score must an integer greater than 0'
+            raise ValueError(error)
+        # Save player's score
+        add = self.__db.insert_one({'name': name,
+                                    'score': score,
+                                    'date': datetime.now()})
+        if not add.acknowledged:
+            error = 'ERROR: Failed to save player\'s score'
+            raise IOError(error)
+
+    def remove(self, name: str, score: int) -> None:
+        """Removes a player's score from the database"""
+        self.isAlive()  # Check connection status before proceeding
+
+        # Data validation
+        if not isinstance(name, str):
+            error = 'ERROR: name must be a string'
+            raise ValueError(error)
+        if not isinstance(score, int):
+            error = 'ERROR: score must be an int'
+            raise ValueError(error)
+        # Remove score
+        remove = self.__db.delete_one({'name': name, 'score': score})
+        if not remove.deleted_count:
+            error = 'ERROR: Failed to remove player\'s score'
+            raise IOError(error)
+
+    def __getattr__(self, item) -> None:
+        """Prevents error when unknown attribute is requested."""
+        print(f'{item} is not a valid attribute of {self.__class__.__name__}')
+        return None
