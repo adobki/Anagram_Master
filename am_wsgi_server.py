97e57615bcf58416b28614ae37c6646f2b7c0a42
Donald Ajaps <ajapon@yahoo.com>
2024-07-02 22:26:50 +0100

    Fix am_wsgi_server.py
    - Remove broken gunicorn references and make waitress the default WSGI package.

--- am_wsgi_server.py
+++ am_wsgi_server.py
@@ -1,27 +1,17 @@
-""" Starts Anagram Master WSGI Web Application Server """
+"""Starts Anagram Master WSGI Web Application Server with waitress"""
+
+from api.api import app as am_api
+from paste.translogger import TransLogger
+from waitress import serve
+
+log_format = (' %(REMOTE_ADDR)s %(REMOTE_USER)s [%(time)s]'
+              ' %(REQUEST_METHOD)s "%(REQUEST_URI)s"'
+              ' %(status)s %(bytes)s\n')
+
+options = {'application':           am_api,
+           'setup_console_handler': False,
+           'logger_name':           'AnagramMaster',
+           'format':                log_format}
 
 if __name__ == "__main__":
-    from os import name
-
-    if name == 'nt':
-        # Actions for Windows OS
-        from api.api import app as am_api
-        from paste.translogger import TransLogger
-        from waitress import serve
-
-        log_format = (' %(REMOTE_ADDR)s %(REMOTE_USER)s [%(time)s]'
-                      ' %(REQUEST_METHOD)s "%(REQUEST_URI)s"'
-                      ' %(status)s %(bytes)s\n')
-
-        options = {'application':           am_api,
-                   'setup_console_handler': False,
-                   'logger_name':           'AnagramMaster',
-                   'format':                log_format}
-
-        serve(TransLogger(**options),
-              port=5555, threads=4, ident='AnagramMaster')
-    else:
-        # Actions for other/UNIX-based operating systems
-        from subprocess import check_output
-
-        check_output('python -m gunicorn --bind :5555 --workers=3 api.api:app')
+    serve(TransLogger(**options), port=5555, threads=4, ident='AnagramMaster')
